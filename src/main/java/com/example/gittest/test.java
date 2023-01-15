f13ed29b2209243ceb9eb11f33077da567c08985
chenchao <2436154191@qq.com>
2023-01-15 14:06:29 +0800

    第二次测试信息

--- src/main/java/com/example/gittest/test.java
+++ src/main/java/com/example/gittest/test.java
@@ -4,5 +4,11 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("helloGit");
+        System.out.println("hello");
+        System.out.println("helloGit");
+        System.out.println("hello");
+        System.out.println("helloGit");
+
+
     }
 }
